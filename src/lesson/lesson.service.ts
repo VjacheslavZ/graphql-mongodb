a601591fda38cdfc19ac8475c9f066203f8736d9
VjacheslavZ <vjacheslav.zemlianiy@fluvius.co>
2021-01-02 17:55:55 +0200

    Validation: Create Lesson Input

--- src/lesson/lesson.service.ts
+++ src/lesson/lesson.service.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm';
 import { v4 as uuid } from 'uuid';
 
 import { Lesson } from './lesson.entity';
+import { CreateLessonInput } from './lesson.input';
 
 @Injectable()
 export class LessonService {
@@ -11,7 +12,12 @@ export class LessonService {
     @InjectRepository(Lesson) private lessonRepository: Repository<Lesson>,
   ) {}
 
-  async createLsson(name, startDate, endDate): Promise<Lesson> {
+  async getLesson(id: string): Promise<Lesson> {
+    return this.lessonRepository.findOne({ id });
+  }
+
+  async createLsson(createLessonInput: CreateLessonInput): Promise<Lesson> {
+    const { name, startDate, endDate } = createLessonInput;
     const lesson = this.lessonRepository.create({
       id: uuid(),
       name,
