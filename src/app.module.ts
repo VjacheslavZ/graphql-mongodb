b43a513b6f9d600fa97151fef0153aa9c3b98369
VjacheslavZ <vjacheslav.zemlianiy@fluvius.co>
2021-01-02 18:38:42 +0200

    Defining the Student Entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { LessonModule } from './lesson/lesson.module';
 import { Lesson } from './lesson/lesson.entity';
+import { StudentModule } from './student/student.module';
+import { Student } from './student/student.entity';
 
 @Module({
   imports: [
@@ -12,12 +14,13 @@ import { Lesson } from './lesson/lesson.entity';
       url: 'mongodb://localhost/school',
       synchronize: true,
       useUnifiedTopology: true,
-      entities: [Lesson],
+      entities: [Lesson, Student],
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
     LessonModule,
+    StudentModule,
   ],
 })
 export class AppModule {}
